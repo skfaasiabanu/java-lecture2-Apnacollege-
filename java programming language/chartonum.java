97af544bf0290014a1b1d28887d55b1649be9ace
SHAIK FAASIA BANU <skfaasiabanu178@gmail.com>
2024-10-31 18:44:32 +0530

    ok

---
+++ java programming language/chartonum.java
@@ -0,0 +1,13 @@
+public class chartonum{
+    public static void main(String args[])
+    {  
+        /*char ch='a';
+        int number=ch;
+        System.out.println(number);
+        */
+       int number=97;
+       char ch=number;
+       System.out.println(ch);
+
+    }
+}
---
+++ java programming language/typecasting.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class typecasting{
+    public static void main(String args[])
+    {
+        
+        float a=12.35f;
+        int b=(int)a;
+        System.out.println(b);
+
+
+    }
+}
